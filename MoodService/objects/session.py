f8123011960ea037dd492f084c5bb1b316cfb214
William Cipriano <williamfcipriano@gmail.com>
2020-04-25 18:42:37 -0400

    working on docker

--- MoodService/objects/session.py
+++ MoodService/objects/session.py
@@ -1,6 +1,12 @@
 class Session:
+    """A session describes a users permission to use the application"""
 
     def __init__(self, user_int_id, session_id, session_token):
         self.user_int_id = user_int_id
         self.session_id = session_id
         self.session_token = session_token
+
+    def __eq__(self, other):
+        if self.session_id == other.session_id:
+            return True
+        return False
--- MoodService/objects/user.py
+++ MoodService/objects/user.py
@@ -5,3 +5,8 @@ class User:
         self.int_id = int_id
         self.username = username
         self.password = password
+
+    def __eq__(self, other):
+        if self.int_id == other.int_id:
+            return True
+        return False
